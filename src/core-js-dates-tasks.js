f8b1eebb33b2d6595a976a8c15c86fb5c9e1f608
liakh83 <lahn4573@gmail.com>
2025-03-22 21:46:21 +0300

    feat: add solution for function  getWeekNumberByDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -202,8 +202,16 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const newDate = new Date(date);
+  const weekDay = (newDate.getDay() + 6) % 7;
+  const thursday = new Date(newDate);
+  thursday.setDate(newDate.getDate() - weekDay + 3);
+  const firstDay = new Date(thursday.getFullYear(), 0, 1);
+  const firstThursday = new Date(firstDay);
+  firstThursday.setDate(firstDay.getDate() + ((4 - firstDay.getDay() + 7) % 7));
+  const differentTime = thursday.getTime() - firstThursday.getTime();
+  return Math.floor(differentTime / (7 * 1000 * 60 * 60 * 24)) + 1;
 }
 
 /**
